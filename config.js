8fb7d6e7e051b1e8da8d97bf31e4b8efacbd5e78
deAYS <99054723+deAYS@users.noreply.github.com>
2025-04-12 05:23:31 +0300

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ module.exports = {
   TOKEN: "",
   language: "en",
   ownerID: ["1004206704994566164", ""], 
-  mongodbUri : "",
+  mongodbUri : "mongodb+srv://deAYS:<db_password>@cluster0.nuyhp4b.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
   spotifyClientId : "",
   spotifyClientSecret : "",
   setupFilePath: './commands/setup.json',
